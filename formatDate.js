ff46c809949ec05080bce364a8055c4b5b5efcfd
Tovmas Petrosyan <tompetrosyan30@gmail.com>
2023-09-23 17:10:19 +0400

    given date, shold be print day, month, year

---
+++ formatDate.js
@@ -0,0 +1,15 @@
+
+function formatDate(date){ 
+    const months = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+      ];
+    
+      const day = date.getDate();
+      const month = months[date.getMonth()];
+      const year = date.getFullYear();
+    
+      return `${day} ${month} ${year}`;
+}
+
+    
