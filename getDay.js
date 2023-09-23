a171c5c2af06950a2cd817c3de84527d5e056b5c
Tovmas Petrosyan <tompetrosyan30@gmail.com>
2023-09-23 16:53:03 +0400

    write function getDay, that return dayofYear

---
+++ getDay.js
@@ -0,0 +1,15 @@
+function getDay(data) {
+    const days = {
+        0: "Sunday",
+        1: "Monday",
+        2: "Tuesday",
+        3: "Wednesday",
+        4: "Thursday",
+        5: "Friday",
+        6: "Saturday"
+    }
+   const day = new Date(data).getDay();
+    return `"${days[day]}"`;
+
+}
+
